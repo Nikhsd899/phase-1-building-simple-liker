da4244898f2e35ddf21d15d3ad1be4bf42baf986
Nicholas <nikhsd899@outlook.com>
2021-11-16 04:45:49 -0800

    commit tree 5ffaab24bb244cb2e55b04395f8bf58a56d60e9f

--- main.js
+++ main.js
@@ -3,10 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const glyphStates = {
+    "♡": "♥",
+    "♥": "♡"
+  };
+  
+  const colorStates = {
+    "red" : "",
+    "": "red"
+  };
 
+const articleHearts = document.querySelectorAll(".like-glyph");
 
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall("bogusUrl", {forceFailure: true})
+    .then(function(){
+       heart.innerText = glyphStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
 
-
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
